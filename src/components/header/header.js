1165457526f9ee36a7158bdbb5fe364e9b48b31c
Ivan Dias <ivandiasworkrelated@gmail.com>
2023-03-25 13:55:23 +0000

    media queries optimization

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,9 +7,15 @@ function Header() {
         <nav className="header-nav">
           <div className="header-name">Ivan.dev</div>
           <ul className="header-list">
-            <li className="header-list-home"><a href="#section-home" >Home</a></li>
-            <li className="header-list-projects"><a href="#section-project">Projects</a></li>
-            <li className="header-list-contact"><a href="#section-contact">Contact</a></li>
+            <li className="header-list-home">
+              <a href="#section-home" >Home</a>
+            </li>
+            <li className="header-list-projects">
+              <a href="#section-project">Projects</a>
+            </li>
+            <li className="header-list-contact">
+              <a href="#section-contact">Contact</a>
+            </li>
           </ul>
         </nav>
       </header>
