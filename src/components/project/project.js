20744d878675b68f6506536252a419d42111e342
Ivan Dias <ivandiasworkrelated@gmail.com>
2023-04-15 11:15:10 +0100

    project add

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,13 +1,12 @@
 import './project.css';
 
-import portfolioThumbnail from '../images/portfolio-thumbnail.png';
+import portfolioThumbnail from '../images/holidayplanner-thumbnail.png';
 import github from '../images/github-black-logo.png';
 import demo from '../images/website.png';
 
 function Project() {
   return (
         <section id="section-project" className="section-project">
-          
           <div className="section-project-grid">
             <div className="project"><h1>Projects</h1></div>
             <div className="project">
@@ -15,49 +14,31 @@ function Project() {
                 <img className="section-project-image" src= {portfolioThumbnail} alt="Portfolio"/>
                 <div className="section-project-info">
                   <div className="section-project-description">
-                    <h2>Portfolio</h2>
-                    <p>In this website you can learn more about me and explore some of the projects I've worked on.<p>This application was built using React.js, which allows for an easier component organization, and while the project is simple it was designed to showcase my skills and experience as a developer.</p></p>
+                    <h2>Holiday Planner</h2>
+                    <p>Our platform offers a range of functionalities to help you plan, update, and delete your vacations with ease. You can effortlessly create plans by providing plan details, whether it's a personal goal or a team vacation. We have exciting features in our roadmap to enhance your experience further. </p>
                   </div>
                   <div className="section-project-skills">
                     <p>HTML5</p>
                     <p>CSS3</p>
+                    <p>JS6</p>
                     <p>React.js</p>
                   </div>
                   <div className="section-project-external">
-                    <a className="a-section-project-github" href="https://github.com/ivandiaswr/ivan-dias-portfolio" target="_blank" rel="noreferrer">Code<img className="img-section-project-github" src= {github} alt="GitHub" title="GitHub"/></a>
-                    <a className="a-section-project-demo" href="https://ivandiaswr.github.io/ivan-dias-portfolio/" target="_blank" rel="noreferrer">Live Demo<img className="img-section-project-demo" src= {demo} alt="Live Demo" title="Live Demo"/></a>
+                    <a className="a-section-project-github" href="https://github.com/ivandiaswr/holiday-planner" target="_blank" rel="noreferrer">
+                      Code
+                      <img className="img-section-project-github" src= {github} alt="GitHub" title="GitHub"/>
+                    </a>
+                    <a className="a-section-project-demo" href="https://victorious-plant-0f4deba03.3.azurestaticapps.net" target="_blank" rel="noreferrer">
+                      Live Demo
+                      <img className="img-section-project-demo" src= {demo} alt="Live Demo" title="Live Demo"/>
+                    </a>
                   </div>
                 </div>
               </div>
             </div>
-
-           
           </div>
         </section>
   );
 }
 
 export default Project;
-
-
-/*  <div className="project">
-              <div className="project-items">
-                <img className="section-project-image" src= "" alt="Under development"/>
-                <div className="section-project-info">
-                <div className="section-project-description">
-                    <h2>Under development</h2>
-                    <p>Under development</p>
-                  </div>
-                  <div className="section-project-skills">
-                    <p>HTML5</p>
-                    <p>CSS3</p>
-                    <p>React.js</p>
-                  </div>
-                  <div className="section-project-external">
-                    <a className="a-section-project-github" href="https://github.com/ivandiaswr" target="_blank" rel="noreferrer">Code<img className="img-section-project-github" src= {github} alt="GitHub" title="Github"/></a>
-                    <a className="a-section-project-demo" href="https://github.com/ivandiaswr" target="_blank" rel="noreferrer">Live Demo<img className="img-section-project-demo" src= {demo} alt="Live Demo" title="Live Demo"/></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            */
